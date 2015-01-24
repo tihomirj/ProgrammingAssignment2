44a00ddbc88a0964669bc166b3d89d94653ffc1e
tihomirj <tihomirj@gmail.com>
2015-01-24 14:51:09 +0200

    programming assignment 2 - from 24Jan2014-officially final

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,19 @@
-# Matrix inversion is time-consuming and it is better to be cached when possible. 
+# Matrix inversion is time-consuming. When the inverse is needed several times
+# and it does not change, it is good to be cached, so that it can be retrieved when 
+# needed.
 # When inversion is done for the first time it is computed and then stored in cache. 
 # When the inverse matrix is needed again it is retrieved from the cache.
 # The functions below create a matrix which inverse is stored in cache for future retrieval.
 
 makeCacheMatrix <- function(x = matrix()) {
 # makeCacheMatrix() returns a list which elements are 
-# four other functions:
+# four functions:
 # 1. set() - sets the value of the matrix
 # 2. get() - gets the value of the matrix
 # 3. setinverse() - sets the inverse of the matrix
 # 4. getinverse() - gets the inverse of the matrix
     
-    cache <- NULL               # the cache, in the beginning it is NULL
+    cache <- NULL               # the cache, in the beginning is NULL
    
     set <- function(y) {        # sets the matrix
         x <<- y
@@ -34,16 +36,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 # cacheSolve() returns the inverse of the matrix. 
-# If the inverse has already been computed the inverse matrix is retrieved from 
-# cache and the computation is skipped. 
-# If the inverse has not been computed yet, the function makes the inverse and 
+# If the inverse has already been computed, the inverse matrix is retrieved from 
+# the cache and the computation is skipped. 
+# If the inverse has not been computed yet, the function does the inverse and 
 # places it in the cache via the setinverse() function.   
         
-    cache <- x$getinverse()     # read the cache
+    cache <- x$getinverse()     # reads the cache
    
-    if(!is.null(cache)) {       # check if the inverse has already been computed 
+    if(!is.null(cache)) {       # checks if the inverse has already been computed if yes 
         message("Retrieving cached data.")
-        return(cache)           # if "yes" returns the stored inverse from the cash
+        return(cache)           # returns the stored inverse from the cache
     }
                                  
     data <- x$get()             # gets the matrix
